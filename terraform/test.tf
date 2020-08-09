0a377e872daeaa2bf9199e47e8c0c6ae27cd721b
CHEVALIER Clement (T0206142) <clement.chevalier@thalesgroup.com>
2020-08-10 00:12:21 +0200

    fix ami

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,8 +1,24 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-03ba3948f6c37a4b0"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
-    Name = "jenkins_builder"
+    Name = "packer_test"
   }
 }
